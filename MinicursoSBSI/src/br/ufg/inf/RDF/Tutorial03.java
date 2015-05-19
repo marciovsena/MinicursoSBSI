db404b086bcd15287221110c45a3027a4a62d830
ErnestoVeiga <ernestofonsecaveiga@gmail.com>
2015-05-19 11:16:29 -0300

    Inclusão de tutoriais RDF e SPARQL

---
+++ MinicursoSBSI/src/br/ufg/inf/RDF/Tutorial03.java
@@ -0,0 +1,87 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package br.ufg.inf.RDF;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
+import com.hp.hpl.jena.vocabulary.VCARD;
+
+/**
+ * Tutorial 3 - Uso de m�todos de acesso a DECLARA��ES (ou STATEMENTS)
+ */
+public class Tutorial03 extends Object {
+	public static void main(String args[]) {
+
+		// String para cria��o de URI de RECURSO
+		String personURI = "http://somewhere/JohnSmith";
+
+		// Strings para cria��o de VALORES DE PROPRIEDADE do tipo LITERAL
+		String givenName = "John";
+		String familyName = "Smith";
+		String fullName = givenName + " " + familyName;
+
+		// Cria��o de um MODELO RDF vazio
+		Model model = ModelFactory.createDefaultModel();
+
+		// Cria��o e associa��o de um RECURSO a um MODELO RDF, e adi��o em
+		// cascata de PROPRIEDADES a esse RECURSO
+		Resource johnSmith = model
+				.createResource(personURI)
+				.addProperty(VCARD.FN, fullName)
+				.addProperty(
+						VCARD.N,
+						model.createResource()
+								.addProperty(VCARD.Given, givenName)
+								.addProperty(VCARD.Family, familyName));
+
+		// Retorna todas as DECLARA��ES (ou TRIPLAS) do MODELO RDF
+		StmtIterator iter = model.listStatements();
+
+		// Cada elemento do StmtIterator � uma TRIPLA, ent�o pode-se obter os
+		// respectivos SUJEITO, PREDICADO e OBJETO de cada TRIPLA
+		while (iter.hasNext()) {
+			Statement stmt = iter.nextStatement(); // obt�m a pr�xima TRIPLA
+			Resource subject = stmt.getSubject(); // obt�m o SUJEITO
+			Property predicate = stmt.getPredicate(); // obt�m o PREDICADO
+			RDFNode object = stmt.getObject(); // obt�m o OBJETO (pode ser uma
+												// URI de RECURSO ou um LITERAL
+												// ==> RDFNode)
+
+			// M�todo toString() retorna uma representa��o em String das URIs de
+			// RECURSOS e PROPRIEDADES
+			System.out.print(subject.toString());
+			System.out.print(" " + predicate.toString() + " ");
+
+			// Verifica��o do tipo de objeto Java: RECURSO (linha 71) ou LITERAL
+			// (linha 73)???
+			if (object instanceof Resource) {
+				System.out.print(object.toString());
+			} else {
+				// OBJETO � um LITERAL
+				System.out.print(" \"" + object.toString() + "\"");
+			}
+			System.out.println(" .");
+		}
+	}
+}
---
+++ MinicursoSBSI/src/br/ufg/inf/SPARQL/Tutorial30.java
@@ -0,0 +1,107 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package br.ufg.inf.SPARQL;
+
+import java.io.InputStream;
+
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.query.ResultSetFormatter;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.util.FileManager;
+
+public class Tutorial30 {
+
+	public static void main(String args[]) {
+
+		// Retorna um grafo RDF inferindo novas triplas e convertendo
+		// propriedade AGE de defini�ao no namespace INFO para defini��o no
+		// namespace FOAF
+		String query = "PREFIX info:    <http://somewhere/peopleInfo#> "
+				+ "PREFIX foaf:    <http://xmlns.com/foaf/0.1/> "
+				+ "PREFIX vcard:      <http://www.w3.org/2001/vcard-rdf/3.0#>"
+				+ "CONSTRUCT " + "{ ?suj   a        foaf:Person ;"
+				+ "         foaf:age ?age ." + "}" + "WHERE "
+				+ "{ ?suj   info:age  ?age " + "}";
+		/*
+		 * querySPARQL - A string de consulta na linguagem SPARQL file = N�mero
+		 * relativo ao arquivo contendo dados RDF
+		 */
+
+		// Fonte de dados: 2 = vc-db-2.rdf
+
+		// TESTE AS DEMAIS FONTES DE DADOS:
+		// Fonte de dados: 3 = vc-db-3.rdf
+		// Fonte de dados: 4 = vc-db-4.rdf
+		// Fonte de dados: 5 = vc-db-5.rdf
+		// Fonte de dados: 6 = vc-db-6.rdf
+
+		queryModel(query, 6);
+	}
+
+	public static void queryModel(String querySPARQL, int file) {
+
+		String inputFileName = null;
+
+		if (file == 2) {
+			inputFileName = "br/ufg/inf/rdf/vc-db-2.rdf";
+		} else if (file == 3) {
+			inputFileName = "br/ufg/inf/rdf/vc-db-3.rdf";
+		} else if (file == 5) {
+			inputFileName = "br/ufg/inf/rdf/vc-db-5.rdf";
+		} else if (file == 6) {
+			inputFileName = "br/ufg/inf/rdf/vc-db-6.rdf";
+		} else {
+			throw new IllegalArgumentException("Arquivo: " + inputFileName
+					+ " n�o encontrado!");
+		}
+
+		// Cria��o de um MODELO RDF vazio
+		Model model = ModelFactory.createDefaultModel();
+
+		// Uso da classe Java FILEMANAGER para localizar o arquivo de entrada
+		InputStream in = FileManager.get().open(inputFileName);
+		if (in == null) {
+			throw new IllegalArgumentException("Arquivo: " + inputFileName
+					+ " nao encontrado");
+		}
+
+		// Leitura do arquivo RDF de entrada na sintaxe padr�o RDF/XML
+		model.read(in, "");
+
+		// Cria��o de um objeto Query com a string de consulta na linguagem
+		// SPARQL
+		Query query = QueryFactory.create(querySPARQL);
+
+		// Interface para a execu��o de uma �nica consulta sobre o GRAFO do
+		// arquivo
+		QueryExecution qe = QueryExecutionFactory.create(query, model);
+
+		// M�todo execConstruct() executa consultas CONSTRUCT e retorna um grafo
+		// RDF
+		Model resultModel = qe.execConstruct();
+		qe.close();
+
+		resultModel.write(System.out, "TURTLE");
+	}
+}
