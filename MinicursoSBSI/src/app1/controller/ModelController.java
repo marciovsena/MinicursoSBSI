4500e95201165829b76f0332bdd1ce1a4545bcbd
marciovsena <marciosena.inf@gmail.com>
2015-05-26 23:33:32 -0300

    atualização da opção 6 Sugestão por área especifica.

--- MinicursoSBSI/src/app1/controller/ModelController.java
+++ MinicursoSBSI/src/app1/controller/ModelController.java
@@ -17,8 +17,8 @@ import com.hp.hpl.jena.util.FileManager;
 public class ModelController {
 	
 	static Model model;
-	static String FOAF = "ontologies/foaf.owl";
-	static String ACM = "ontologies/acm.owl";
+	static String FOAF = "src/ontologies/foaf.owl";
+	static String ACM = "src/ontologies/acm.owl";
 
 
 	public static void inicializaModelo() {
